d75a4ad9cdcc32cdf48730e254a6fb5425d5f934
Aaron Sung <aaronsung@gmail.com>
2024-08-29 16:04:31 +0800

    Added React Router

--- public/electron.js
+++ public/electron.js
@@ -24,7 +24,7 @@ function createWindow() {
   });
 
   mainWindow.loadURL(startUrl);
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p>This is a sample project demonstrating the use of React, React Router, and Electron.</p>
+    </div>
+  );
+}
+
+export default About;
